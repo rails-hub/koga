d67047b51bf25faadb0cdbb00d2647addde55fdf
todd <todd@todd.(none)>
2014-09-10 15:43:57 +0500

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Koga::Application.config.secret_token = 'a7db580016b50905743c6cf19d039630d09ccc0f8d30e75c5ae8140f6f0dd244f4cc35de8b892685dc68e8079b5fc8b8eaf4a7a81e670480452b29e815fb42fb'
